83eb9ae228e7893d0a9cd648e94ddad16fb040ac
NurKeinNeid <mralexman3000@gmail.com>
2019-12-24 00:18:06 +0100

    Revert "pixelstyle: Remove PixelThemesStub2019"
    This reverts commit 24d55e118b1353c9ae5254c01115feef2474b373.
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config.mk
+++ config.mk
@@ -18,6 +18,7 @@ LOCAL_PATH := vendor/pixelstyle
 PRODUCT_PACKAGES += \
     NexusLauncherRelease \
     NexusWallpapersStubPrebuilt2019Static \
+    PixelThemesStub2019 \
     OPScreenRecorder \
     SafetyHubPrebuilt \
     SettingsIntelligenceGooglePrebuilt \
